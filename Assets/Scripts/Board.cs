08d9b36098ba4e0d8c95f32c5d523a527927897a
Amlan <khuntia.amlan@gmail.com>
2021-09-12 15:39:36 +0530

    Candy Crush initial setup

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject[] dots;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+    public GameObject[,] allDots;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
+        Setup();
+    }
+    private void Setup()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for(int j =0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backfroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backfroundTile.transform.parent = this.transform;
+                backfroundTile.name = "( " + i + ", " + j + ")";
+                int dotToUse = Random.Range(0, dots.Length);
+                int maxIterations = 0;
+                while (MatchesAt(i, j, dots[dotToUse]) && maxIterations < 100)
+                {
+                    dotToUse = Random.Range(0, dots.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + ", " + j + ")";
+                allDots[i, j] = dot;
+            }
+        }
+    }
+
+    private bool MatchesAt(int column, int row, GameObject piece)
+    {
+        if (column >1 && row > 1)
+        {
+            if (allDots[column-1,row].tag == piece.tag && allDots [column-2,row].tag == piece.tag)
+            {
+                return true;
+            }
+            if (allDots[column , row-1].tag == piece.tag && allDots[column , row-2].tag == piece.tag)
+            {
+                return true;
+            }
+        }
+        else if(column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+          
+            }
+            if (column > 1)
+            {
+                if (allDots[column-1, row ].tag == piece.tag && allDots[column-2, row ].tag == piece.tag)
+                {
+                    return true;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+}
